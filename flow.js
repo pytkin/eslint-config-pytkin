264203c6777f8f326e39a7c65f17e938c8cba7b5
Dmitry Pytkin <tutbaker@gmail.com>
2017-08-22 03:02:19 +0300

    Create flow.js

---
+++ flow.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: "babel-eslint",
+
+  // https://github.com/gajus/eslint-plugin-flowtype
+  plugins: ["flowtype"],
+
+  extends: ["./index.js"],
+
+  rules: {
+    // avoid false positive eslint errors
+    "flowtype/define-flow-type": 2,
+    "flowtype/use-flow-type": 2,
+
+    "flowtype/boolean-style": 2,
+    "flowtype/no-dupe-keys": 2,
+    "flowtype/no-primitive-constructor-types": 2,
+    "flowtype/type-id-match": [2, "^([A-Za-z][A-Za-z0-9]*)+Type$"],
+
+    // too brutal
+    "flowtype/no-types-missing-file-annotation": 0, // what about flow 'all' option?
+    "flowtype/no-weak-types": 0,
+    "flowtype/require-parameter-type": 0,
+    "flowtype/require-return-type": 0,
+    "flowtype/require-valid-file-annotation": 0,
+    "flowtype/require-variable-type": 0,
+    "flowtype/sort-keys": 0,
+
+    // no styles rules (=> prettier)
+    "flowtype/semi": 0,
+    "flowtype/delimiter-dangle": 0,
+    "flowtype/object-type-delimiter": 0,
+    "flowtype/generic-spacing": 0,
+    "flowtype/space-after-type-colon": 0,
+    "flowtype/space-before-generic-bracket": 0,
+    "flowtype/space-before-type-colon": 0,
+    "flowtype/union-intersection-spacing": 0,
+
+    // deprecated
+    "flowtype/valid-syntax": 0
+  },
+};
